61bb744972a27e9d65cdb9023acae5ffe5331d45
Khang Nguyen <nguyendakcun@gmail.com>
2024-03-03 21:12:02 +0700

    bai kiem tra

---
+++ baikiemtra.js
@@ -0,0 +1,16 @@
+const ax1 = parseInt(document.querySelector('#ax1').value);
+const b1 = parseInt(document.querySelector('#b1').value);
+const c1 = parseInt(document.querySelector('#c1').value);
+const c2 = parseInt(document.querySelector('#c2').value);
+const ax2 = parseInt(document.querySelector('#ax2').value);
+const b2 = parseInt(document.querySelector('#b2').value);
+const x0 = parseInt(document.querySelector('#x0').value);
+const result1 = document.querySelector('#result1');
+const result2 = document.querySelector('#result2');
+const result3 = document.querySelector('#result3');
+const result4 = document.querySelector('#result4');
+const result5 = document.querySelector('#result5');
+result1.textContent = ax1+'x + '+b1 +' = '+c1
+result2.textContent = ax2+'x + '+b2 +' = '+c2
+result3.textContent = ax2+'x + '+b2 +' = '+c2
+result4.textContent = ax1+x0+b2 
